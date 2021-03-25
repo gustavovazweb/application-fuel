aff4ca9723c6cccfe0e836438312cd7917579730
Gustavo Vaz <68481189+gustavovazweb@users.noreply.github.com>
2021-03-25 20:18:24 -0300

    Commit - Upload module files

---
+++ app/app-routing.module.ts
@@ -0,0 +1,15 @@
+//IMPORTING MODULES
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+//ROUTES
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
